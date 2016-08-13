ddce3b1f57d3e33b6fa5fbc33b3714b0660b5709
Stokori <stokoriel@gmail.com>
2016-08-13 00:29:49 -0500

    More housekeeping

--- mapkov.py
+++ mapkov.py
@@ -19,28 +19,31 @@ width = 300
 height = 300
 
 # Sample text
-sampleText = ("Stop. Who would cross the Bridge of Death must answer me these questions three,"
-                " ere the other side he see. Ask me the questions, bridgekeeper."
-                " I am not afraid. What... is your name? My name is Sir Lancelot of Camelot."
-                " What... is your quest? To seek the Holy Grail. What... is your favourite colour?"
-                " Blue. Go on. Off you go. Oh, thank you. Thank you very much. That's easy."
-                " Stop. Who would cross the Bridge of Death must answer me these questions three, ere"
-                " the other side he see. Ask me the questions, bridgekeeper. I'm not afraid."
-                " What... is your name? Sir Robin of Camelot. What... is your quest?"
-                " To seek the Holy Grail. What... is the capital of Assyria? [pause]"
-                " I don't know that. [he is thrown over the edge into the volcano]"
-                " Auuuuuuuugh. Stop. What... is your name? Sir Galahad of Camelot. What... is your quest?"
-                " I seek the Grail. What... is your favourite colour? Blue. No, yel..."
-                " [he is also thrown over the edge] auuuuuuuugh. Hee hee heh. Stop."
-                " What... is your name? It is 'Arthur', King of the Britons."
-                " What... is your quest? To seek the Holy Grail. What... is the air-speed velocity of an unladen swallow?"
-                " What do you mean? An African or European swallow? Huh? I... I don't know that."
-                " [he is thrown over] Auuuuuuuugh. How do know so much about swallows?"
-                " Well, you have to know these things when you're a king, you know.")
-                
-                
+sampleText = ("Stop. Who would cross the Bridge of Death must answer me these qu"
+              "estions three, ere the other side he see. Ask me the questions, b"
+              "ridgekeeper. I am not afraid. What... is your name? My name is Si"
+              "r Lancelot of Camelot. What... is your quest? To seek the Holy Gr"
+              "ail. What... is your favourite colour? Blue. Go on. Off you go. O"
+              "h, thank you. Thank you very much. That's easy. Stop. Who would c"
+              "ross the Bridge of Death must answer me these questions three, er"
+              "e the other side he see. Ask me the questions, bridgekeeper. I'm "
+              "not afraid. What... is your name? Sir Robin of Camelot. What... i"
+              "s your quest? To seek the Holy Grail. What... is the capital of A"
+              "ssyria? [pause] I don't know that. [he is thrown over the edge in"
+              "to the volcano] Auuuuuuuugh. Stop. What... is your name? Sir Gala"
+              "had of Camelot. What... is your quest? I seek the Grail. What... "
+              "is your favourite colour? Blue. No, yel... [he is also thrown ove"
+              "r the edge] auuuuuuuugh. Hee hee heh. Stop. What... is your name?"
+              "It is 'Arthur', King of the Britons. What... is your quest? To s"
+              "eek the Holy Grail. What... is the air-speed velocity of an unlad"
+              "en swallow? What do you mean? An African or European swallow? Huh"
+              "? I... I don't know that. [he is thrown over] Auuuuuuuugh. How do"
+              "know so much about swallows? Well, you have to know these things"
+              "when you're a king, you know.")
+
+
 def buildFrequencyTable(text):
-    """ Build a table pairing each word in the sample text with what word(s) are most likely to come after it. """
+    """ Pair each word in the sample text with what word(s) are most likely to come after it. """
 
     # Turn the input text into a list of words/tokens
     sourceList = text.split()
@@ -76,22 +79,29 @@ def getColorFromString(baseString):
     """ Take an arbitrary string and return a RGB tuple from the first 3 bytes. """
 
     # Get byte array of string
-    hash = 0
-    
+    stringHash = 0
+
     # Hash it together using the sdbm algorithm
     for c in baseString:
-        hash = hash * (ord(c) - 1) * 65599 + ord(c)
-        
-    rgb = [0,0,0]
+        stringHash = stringHash * (ord(c) - 1) * 65599 + ord(c)
+
+    rgb = [0, 0, 0]
 
     # Split it up into 3 separate values
     for i in range(3):
-        rgb[i] = int((hash >> (i * 8)) & 0xFF)
+        rgb[i] = int((stringHash >> (i * 8)) & 0xFF)
 
     return tuple(rgb)
 
 def findNextWord(currentWord, markovModel):
     """ Use our frequency table to get the next word in sequence. """
+
+    print(currentWord)
+
+    # If this line was
+    if not markovModel[currentWord]:
+        while not markovModel[currentWord]:
+            currentWord = random.sample(markovModel.items(), 1)[0][0]
 
     potentialWords = []
 
@@ -106,6 +116,27 @@ def findNextWord(currentWord, markovModel):
 
     return nextWord
 
+def drawTile(screen, markovModel, word, coordinates):
+    """ Draws a colored tile on the screen at the specified coordinates. """
+
+    # Find our next word and its respective color
+    word = findNextWord(word, markovModel)
+    color = getColorFromString(word)
+
+    # Draw the tile
+    pygame.draw.rect(screen, color, pygame.Rect(coordinates[0], coordinates[1], 5, 5))
+    pygame.display.flip()
+
+    # Change x and y so the next square is in the right place.
+    # If we're at the edge of the screen, loop to the next row.
+    coordinates[0] += 5
+    if coordinates[0] > width:
+        coordinates[0] = 0
+        coordinates[1] += 5
+
+    print(word, coordinates)
+
+    return [word, coordinates]
 
 def main():
     """ Creates pygame window and runs the main process """
@@ -125,32 +156,13 @@ def main():
     word = random.sample(markovModel.items(), 1)[0][0]
 
     # Our first square will be at 0,0
-    x = 0
-    y = 0
+    coordinates = [0, 0]
 
     for i in range(numberOfSquares):
 
-        # If our next word doesn't have any words after it, get us a new one.
-        if not markovModel[word]:
-            while not markovModel[word]:
-                word = random.sample(markovModel.items(), 1)[0][0]
-
-        # Find our next word and its respective color
-        word = findNextWord(word, markovModel)
-        color = getColorFromString(word)
-
-        # Draw the tile
-        pygame.draw.rect(screen, color, pygame.Rect(x, y, 5, 5))
-        pygame.display.flip()
-
-        # Change x and y so the next square is in the right place.
-        # If we're at the edge of the screen, loop to the next row.
-        x += 5
-        if x > width:
-            x = 0
-            y += 5
-
-        print(word, color)
+        tile = drawTile(screen, markovModel, word, coordinates)
+        word = tile[0]
+        coordinates = tile[1]
 
     print("Done!")
 
